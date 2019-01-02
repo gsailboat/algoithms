242777c252fa6001ecc86807d428c2d736e666ab
Greg Selbo <gregselbo@Gregs-MacBook-Pro.local>
2019-01-02 14:16:21 -0500

    Adding a fun one

---
+++ digits.js
@@ -0,0 +1,17 @@
+let answer = 0;
+
+var addDigits = function(num) {
+	let length = num.toString().length;
+	let current = 0;
+
+	for (let i = length; i > 0; i--) {
+		current += num % 10;
+		num = Math.floor(num / 10);
+	}
+	answer = current;
+	if (answer < 10)
+		return answer;
+	return addDigits(answer);	
+};
+
+console.log(addDigits(447));
